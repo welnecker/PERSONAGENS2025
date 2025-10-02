8476f54ac3eb071f9e20a14d433cf763f47e7f34
welnecker <janiofdr@gmail.com>
2025-10-02 09:52:53 -0300

    Update __init__.py

--- core/common/__init__.py
+++ core/common/__init__.py
@@ -1,1 +1,13 @@
+# reexporta utilidades comuns
+from .base_service import BaseCharacter
+from .sidebar_types import (
+    FieldSpec, SidebarSection,
+    field_bool, field_text, field_select, field_slider
+)
+
+__all__ = [
+    "BaseCharacter",
+    "FieldSpec", "SidebarSection",
+    "field_bool", "field_text", "field_select", "field_slider",
+]
 
