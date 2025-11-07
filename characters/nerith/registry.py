8533941e536e9f4d08958cef4784f9137e82500d
welnecker <janiofdr@gmail.com>
2025-11-07 13:05:23 -0300

    Create registry.py

---
+++ characters/nerith/registry.py
@@ -0,0 +1,10 @@
+# characters/nerith/registry.py
+
+# Liste aqui todos os modelos que quer ver no seletor da Nerith
+MODELS = [
+    "HF • FLUX.1-dev",
+    "HF • SDXL (nscale)",   # <= novo
+]
+
+def list_models():
+    return MODELS
