f71b770dfc71b846e1474135d2aaa0bfa84ab360
welnecker <janiofdr@gmail.com>
2025-11-12 09:31:03 -0300

    Update __init__.py

--- characters/adelle/__init__.py
+++ characters/adelle/__init__.py
@@ -1,18 +1,2 @@
-characters/
-  __init__.py
-  registry.py
-  adelle/
-    __init__.py
-    service.py
-    persona.py   (opcional)
-  laura/
-    __init__.py
-    service.py
-    persona.py   (opcional)
-  mary/
-    __init__.py
-    service.py
-  nerith/
-    __init__.py
-    service.py
+__all__ = []
 
